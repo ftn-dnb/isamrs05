93430954c5c4439da06152445b76621edea621fa
Nemanja Mikovic <mikovic.nm@gmail.com>
2019-03-23 19:51:19 +0100

    Inicijalni commit

---
+++ dnb-travel/src/main/java/ftn/dnb/dnbtravel/DnbTravelApplication.java
@@ -0,0 +1,13 @@
+package ftn.dnb.dnbtravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnbTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnbTravelApplication.class, args);
+	}
+
+}
