23dd204e73fdeeccd8c5a7f65e41e41327c4777a
Boris Sulicenko <b.sulicenko@gmail.com>
2019-03-26 21:00:53 +0100

    Dodat model,servis,kontroler i repozitorijum za aviokompanije (Airline). Omoguceno dodavanje i izmena podataka aviokompanije

---
+++ dnb-travel/src/main/java/ftn/dnb/dnbtravel/repository/AirlineRepository.java
@@ -0,0 +1,17 @@
+package ftn.dnb.dnbtravel.repository;
+
+import ftn.dnb.dnbtravel.model.Airline;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AirlineRepository extends JpaRepository<Airline, Long> {
+
+    List<Airline> findAll();
+
+    Page<Airline> findAll(Pageable pageable);
+
+    Airline findOneById(Long id);
+}
