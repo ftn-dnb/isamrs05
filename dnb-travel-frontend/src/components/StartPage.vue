4fec0ec3e7c94ebc03768c3dd979234a458e90fe
Boris Sulicenko <b.sulicenko@gmail.com>
2019-03-28 20:11:53 +0100

    Dodat novi Vue.js projekat. Prebacen dosadasnji posao na novu aplikaciju i obrisani su fajlovi stare aplikacije.

---
+++ dnb-travel-frontend/src/components/StartPage.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1>Hello from Vue.js</h1>
+
+		<!-- Ove 2 komponente ovde samo stoje zbog testiranja, obrisati kad se zavrsi sa njihovim radom -->
+        <user-settings></user-settings>
+		<airline-settings></airline-settings>
+    </div>
+</template>
+
+<script>
+import UserSettings from './UserSettings.vue';
+import AirlineSettings from './AirlineSettings.vue';
+
+export default {
+    name: 'StartPage',
+    props: {},
+    components: {
+		UserSettings,
+		AirlineSettings,
+    },
+
+    data() {
+        return {
+
+        };
+    },
+
+    methods: {
+
+    },
+
+    mounted() {
+
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
