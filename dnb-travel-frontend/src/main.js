bf07dd7314c41537374bf0aa0467bae77875cc42
Boris Sulicenko <b.sulicenko@gmail.com>
2019-05-02 22:15:28 +0200

    Redizajn komponente za dodavanje letova, treba srediti date and time picker

--- dnb-travel-frontend/src/main.js
+++ dnb-travel-frontend/src/main.js
@@ -4,10 +4,13 @@ import App from './App.vue'
 import router from './router'
 import VueKonva from 'vue-konva'
 import Toasted from 'vue-toasted';
+import DatetimePicker from 'vuetify-datetime-picker'
+import 'vuetify-datetime-picker/src/stylus/main.styl'
 
 Vue.config.productionTip = false
 Vue.use(VueKonva)
 Vue.use(Toasted)
+Vue.use(DatetimePicker)
 
 new Vue({
   router,
