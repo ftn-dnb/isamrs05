7112abd338cba568e81bfda94ba4b40b749f45d0
Boris Sulicenko <b.sulicenko@gmail.com>
2019-05-03 13:28:46 +0200

    Redizajnirana stranica za prikaz svih letova i njihovu pretragu i filtriranje

--- dnb-travel-frontend/src/components/AddFlight.vue
+++ dnb-travel-frontend/src/components/AddFlight.vue
@@ -187,7 +187,7 @@
 <script>
 
 import axios from 'axios';
-import format from 'date-fns/format'
+import format from 'date-fns/format';
 
 export default {
     name: 'AddFlight',
--- dnb-travel-frontend/src/components/FlightList.vue
+++ dnb-travel-frontend/src/components/FlightList.vue
@@ -1,119 +1,154 @@
 <template>
     <div>
-        <table>
-            <tr>
-                <td>
-                    From <br />
-                    <select v-model="filterSearch.startDestination" >
-                        <option :value="null">Clear</option>
-                        <option v-for="dest in destinations" :value="dest">
-                            {{dest.airportName}}, {{dest.city}}, {{dest.country}}
-                        </option>
-                    </select>
-                </td>
-                <td>
-                    To <br />
-                    <select v-model="filterSearch.endDestination">
-                        <option :value="null">Clear</option>
-                        <option v-for="dest in destinations" :value="dest">
-                            {{dest.airportName}}, {{dest.city}}, {{dest.country}}
-                        </option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Depart <br />
-                    <input type="date" v-model="filterSearch.startDate" />
-                </td>
-                <td>
-                    Return <br />
-                    <input type="date" v-model="filterSearch.endDate" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Max duration {{filterSearch.travelTime}}h <br />
-                    <input type="range" v-model="filterSearch.travelTime" min="0" max="50" />
-                </td>
-                <td>
-                    Max length {{filterSearch.travelLength}}km <br />
-                    <input type="range" v-model="filterSearch.travelLength" min="0" max="20000" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="checkbox" v-model="filterSearch.isDirect" /> Direct flight
-                </td>
-                <td>
-                    <input type="checkbox" v-model="filterSearch.isOneWay" /> One way 
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Min. price <br />
-                    <input type="number" v-model="filterSearch.minPrice" placeholder="Minimum" min="0" />
-                </td>
-                <td>
-                    Max. price <br />
-                    <input type="number" v-model="filterSearch.maxPrice" placeholder="Maximum" min="0" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Number of people <br />
-                    <input type="number" v-model="filterSearch.numOfPeople" min="0" />
-                </td>
-                <td>
-                    Airline <br />
-                    <select v-model="filterSearch.airlineId">
-                        <option :value="null">Clear</option>
-                        <option v-for="airline in airlines" :value="airline.id">
-                            {{airline.name}}
-                        </option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2" align="center">
-                    <input type="button" @click="searchFlights" value="Search" />
-                </td>
-            </tr>
-        </table>
+        <v-container grid-list-md text-xs-center>
+            <v-layout row wrap>
+                <v-flex xs4>
+                    <v-combobox label="Select start destination" 
+                        v-model="filterSearch.startDestination" 
+                        :items="destinations" 
+                        item-text="airportName" 
+                        return-object
+                        prepend-icon="flight_takeoff"
+                    ></v-combobox>
 
-        <table border="1">
-            <tr>
-                <th>Start destination</th>
-                <th>End destination</th>
-                <th>Take-off date & time</th>
-                <th>Landing date & time</th>
-                <th>Length (km)</th>
-                <th>Travel time (h)</th>
-                <th>Num. of transits</td>
-                <th v-if="airlineId == null">Airline</th>
-                <th>Rating</th>
-                <th>&nbsp;</th>
-            </tr>
-            <tr v-for="flight in flights">
-                <td>{{flight.startDestination.city}}, {{flight.startDestination.country}}</td>
-                <td>{{flight.endDestination.city}}, {{flight.endDestination.country}}</td>
-                <td>{{flight.startDateTime}}</td>
-                <td>{{flight.endDateTime}}</td>
-                <td>{{flight.travelLength}}</td>
-                <td>{{flight.travelTime}}</td>
-                <td>{{flight.transits.length}}</td>
-                <td v-if="airlineId == null">{{flight.airlineName}}</td>
-                <td>{{flight.rating}}</td>
-                <td>
-                    <router-link :to="{ name: 'FlightDetailedView', params: { flightId: flight.id } }">See details</router-link>
-                </td>
-            </tr>
-        </table>
+                    <v-menu>
+                        <v-text-field label="Departure date" 
+                            slot="activator" 
+                            :value="formattedDate(filterSearch.startDate)"
+                            prepend-icon="event"
+                        ></v-text-field>
+                        <v-date-picker v-model="filterSearch.startDate">
+                        </v-date-picker>
+                    </v-menu>
+
+                    <v-slider
+                        label="Travel time (h)"
+                        v-model="filterSearch.travelTime"
+                        thumb-label="always"
+                        :thumb-size="24"
+                        min="0"
+                        max="50"
+                    ></v-slider>
+
+                    <v-checkbox
+                        label="Direct flight"
+                        v-model="filterSearch.isDirect"
+                    ></v-checkbox>
+
+                    <v-text-field type="number"
+                        label="Min. price"
+                        min="0"
+                        v-model="filterSearch.minPrice"
+                        prepend-icon="attach_money"
+                    ></v-text-field>
+
+                    <v-text-field type="number"
+                        label="Number of people"
+                        v-model="filterSearch.numOfPeople"
+                        min="0"
+                        prepend-icon="people"
+                    ></v-text-field>
+
+                </v-flex>
+                <v-flex xs4>
+                    <v-combobox label="Select end destination" 
+                        v-model="filterSearch.endDestination" 
+                        :items="destinations" 
+                        item-text="airportName" 
+                        return-object
+                        prepend-icon="flight_land"
+                    ></v-combobox>
+
+                    <v-menu>
+                        <v-text-field label="Return date" 
+                            slot="activator" 
+                            :value="formattedDate(filterSearch.endDate)"
+                            prepend-icon="event"
+                        ></v-text-field>
+                        <v-date-picker v-model="filterSearch.endDate">
+                        </v-date-picker>
+                    </v-menu>
+
+                    <v-slider
+                        label="Travel length (km)"
+                        v-model="filterSearch.travelLength"
+                        thumb-label="always"
+                        min="0"
+                        max="20000"
+                    ></v-slider>
+
+                    <v-checkbox
+                        label="One way"
+                        v-model="filterSearch.isOneWay"
+                    ></v-checkbox>
+
+                    <v-text-field type="number"
+                        label="Max. price"
+                        min="0"
+                        v-model="filterSearch.maxPrice"
+                        prepend-icon="attach_money"
+                    ></v-text-field>
+
+                    <v-combobox label="Select airline" 
+                        v-model="airline" 
+                        :items="airlines" 
+                        item-text="name" 
+                        prepend-icon="flight"
+                    ></v-combobox>
+
+                    <v-btn small @click="searchFlights">
+                        <v-icon left>search</v-icon>
+                        <span>Search</span>
+                    </v-btn>
+                </v-flex>
+            </v-layout>   
+        </v-container>
+
+        <v-layout row wrap>
+            <v-flex xs12 sm6 md4 lg3 v-for="flight in flights" :key="flight.id">
+                <v-card flat class="text-xs-center ma-3">
+                    <v-card-text>
+                        <div class="subheading">
+                            {{flight.startDestination.city}}, {{flight.startDestination.country}}
+                        </div>
+                        <div class="grey--text">
+                            {{flight.startDestination.airportName}}
+                        </div>
+                        <div>
+                            <v-icon>flight_takeoff</v-icon>
+                            {{formattedDate(flight.startDateTime)}}
+                        </div>
+
+                        <v-icon>expand_more</v-icon>
+                        
+                        <div class="subheading">
+                            {{flight.endDestination.city}}, {{flight.endDestination.country}}
+                        </div>
+                        <div class="grey--text">
+                            {{flight.endDestination.airportName}}
+                        </div>
+                        
+                        <div>
+                            <v-icon>flight_land</v-icon>
+                            {{formattedDate(flight.endDateTime)}}
+                        </div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn flat small router :to="{ name: 'FlightDetailedView', params: { flightId: flight.id} }">
+                            <v-icon left>info</v-icon>
+                            <span>See details</span>
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
     </div>
 </template>
 
 <script>
+
 import axios from 'axios';
+import format from 'date-fns/format';
 
 export default {
     name: 'FlightList',
@@ -129,6 +164,7 @@ export default {
             airlines: [],
             flights: [],
             destinations: [],
+            airline: null,
             filterSearch: {
                 airlineId: null,
                 startDestination: null,
@@ -148,9 +184,15 @@ export default {
 
     methods: {
         searchFlights() {
+            this.filterSearch.airlineId = this.airline.id;
+
             axios.post('http://localhost:8080/api/flights', this.filterSearch)
             .then(response => {this.flights = response.data; })
             .catch(error => this.$toasted('Error while loading flights', {duration:5000}));
+        },
+
+        formattedDate(date) {
+            return date ? format(date, 'Do MMM YYYY') : '';
         },
     },
 
